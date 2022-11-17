b5ea5bd4a33e72885a72885fcf56315bc267026c
GjergjiSh <gjergji_shkurti@yahoo.com>
2022-11-17 18:23:01 +0100

    logging in ModuleManager

--- rushb/modules/modulemanager/ModuleManager.py
+++ rushb/modules/modulemanager/ModuleManager.py
@@ -1,14 +1,8 @@
 from rushb.modules.collection.IModuleInterface import *
 from rushb.modules.factory.ModuleFactory import make_module
 
-import os
 import yaml
 import logging
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="{asctime} {levelname:<8} {message}",
-    style="{")
 
 class ModuleManger():
     def __init__(self, cfg_path: str) -> None:
@@ -22,39 +16,37 @@ class ModuleManger():
         try:
             for module in self.modules:
                 module.init()
-        except Exception as ex:
-
-            print(ex)
+        except Exception:
+            logging.critical("", exc_info=True)
 
     def deinit(self) -> None:
         """ Deinitialize all the assigned modules """
         try:
             for module in self.modules:
                 module.deinit()
-        except Exception as ex:
-            print(ex)
+        except Exception:
+            logging.critical("", exc_info=True)
 
     def run(self) -> None:
         """ Start processing the modules in a loop """
         try:
             while True:
                 self.__step()
-        except KeyboardInterrupt as ex:
+        except KeyboardInterrupt:
             logging.info("Exiting...")
 
     def __read_config(self):
         """ Read the module configuration """
-        with open(self.cfg_path, "r") as stream:
-            try:
+        try:
+            with open(self.cfg_path, "r") as stream:
                 yaml_file = yaml.safe_load(stream)
-            except yaml.YAMLError as ex:
-                print(ex)
+        except Exception:
+            logging.critical("", exc_info=True)
 
         return yaml_file
 
     def __assign_modules(self, config : dict):
         """ Assign a module to the manager """
-
         for module_name in config:
             module = make_module(module_name,
                                  **config[module_name])
@@ -65,5 +57,5 @@ class ModuleManger():
         try:
             for module in self.modules:
                 module.step()
-        except Exception as ex:
-            print(ex)
+        except Exception:
+            logging.critical("", exc_info=True)
