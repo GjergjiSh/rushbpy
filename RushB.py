cb817390c39fb3e2d457f448b7f38f1e8b48bdb2
GjergjiSh <gjergji_shkurti@yahoo.com>
2022-11-17 18:22:46 +0100

    renamed main to RushB

--- RushB.py
+++ RushB.py
@@ -1,9 +1,15 @@
 from rushb.modules.modulemanager.ModuleManager import *
 from rushb.modules.factory.ModuleFactory import *
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="{asctime} {levelname:<8} {message}",
+    style="{",
+    filename="%slog" % __file__[:-2],
+    filemode="w"),
 
 def main():
-    cfg_path = "C:/Users/Gjergji/Repos/rushbpy/modules.yml"
+    cfg_path = "C:/Users/Gjergji/Repos/rushbpy/modules.ymlx"
     module_manager = ModuleManger(cfg_path)
     module_manager.init()
     try:
